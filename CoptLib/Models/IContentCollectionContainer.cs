5504a9bbd16a7e4bb134d405551aadcbc09cf084
Yoshi Askharoun <jjask7@gmail.com>
2022-11-05 19:55:34 -0500

    Made IContentCollectionContainer implement IDefinition for consistency with IContent

--- CoptLib/Models/IContentCollectionContainer.cs
+++ CoptLib/Models/IContentCollectionContainer.cs
@@ -5,7 +5,7 @@ namespace CoptLib.Models
     /// <summary>
     /// Interface for any model that contains a collection of <see cref="ContentPart"/>s.
     /// </summary>
-    public interface IContentCollectionContainer : ICollection<ContentPart>
+    public interface IContentCollectionContainer : ICollection<ContentPart>, IDefinition
     {
         /// <summary>
         /// A command used to populate the collection. May contain nested text commands.
