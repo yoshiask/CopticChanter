ff574dd08af716e87f3bf56ea0c257e9e8a8b9a0
Yoshi Askharoun <jjask7@gmail.com>
2022-11-14 19:42:33 -0600

    Added tests for DocSetReader/Writer

--- CoptTest/Writer.cs
+++ CoptTest/Writer.cs
@@ -1,6 +1,5 @@
 ﻿using CoptLib.IO;
 using CoptLib.Models;
-using CoptLib.Writing;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -37,10 +36,53 @@ namespace CoptTest
 
             Assert.Equal(docEx.Name, docAc.Name);
             Assert.Equal(docEx.Uuid, docAc.Uuid);
-            Assert.Equal(docEx.Parent, docAc.Parent);
+            Assert.Equal(docEx.Author?.FullName, docAc.Author?.FullName);
             Assert.Equal(docEx.Definitions.Count, docAc.Definitions.Count);
             Assert.Equal(docEx.DirectDefinitions.Count, docAc.DirectDefinitions.Count);
             Assert.Equal(docEx.Translations.Children.Count, docAc.Translations.Children.Count);
+        }
+
+        [Fact]
+        public void DocSetWriter_CreateFromDocList()
+        {
+            string pathActual = _resPrefix + "..\\Output\\test_set.zip";
+            string pathExpected = _resPrefix + "test_set.zip";
+            string[] fileNames = new[]
+            {
+                "Let Us Praise the Lord.xml",
+                "The First Hoos Lobsh.xml",
+                "Watos Hymn for the Three Saintly Children.xml"
+            };
+
+            List<Doc> docs = new(fileNames.Select(f => DocReader.ReadDocXml(_resPrefix + f)));
+            DocSetWriter setWriter = new("test-set", "Test Set", docs);
+            setWriter.Set.Author = new()
+            {
+                FullName = "Yoshi Askharoun",
+                Email = "jjask7@gmail.com",
+                Website = "https://github.com/yoshiask"
+            };
+            setWriter.Write(pathActual);
+
+            DocSet setActual;
+            using (DocSetReader readerActual = new(pathActual))
+            {
+                readerActual.ReadAll();
+                setActual = readerActual.Set;
+            }
+
+            DocSet setExpected;
+            using (DocSetReader readerExpected = new(pathExpected))
+            {
+                readerExpected.ReadAll();
+                setExpected = readerExpected.Set;
+            }
+
+            Assert.Equal(setExpected.Name, setActual.Name);
+            Assert.Equal(setExpected.Uuid, setActual.Uuid);
+            Assert.Equal(setExpected.Author?.Email, setActual.Author?.Email);
+            Assert.Equal(setExpected.Author?.Website, setActual.Author?.Website);
+            Assert.Equal(setExpected.IncludedDocs.Count, setActual.IncludedDocs.Count);
         }
     }
 }
