e6ae8664a02d8e1618cfe46d344119663b097fa5
Yoshi Askharoun <jjask7@gmail.com>
2022-11-21 23:15:29 -0600

    Recursively apply IpaTranscribeCommand

--- CoptLib/Scripting/Commands/IpaTranscribeCmd.cs
+++ CoptLib/Scripting/Commands/IpaTranscribeCmd.cs
@@ -14,13 +14,22 @@ namespace CoptLib.Scripting.Commands
 
         private void Parse(string cmd, IContent content, params IDefinition[] parameters)
         {
-            Output = parameters[0].Select(def =>
-            {
-                if (def is not IContent content)
-                    return;
+            Output = parameters[0].Select(Transcribe);
+        }
 
+        private void Transcribe(IDefinition def)
+        {
+            if (def is IContentCollectionContainer contentCollection)
+            {
+                var newCollection = contentCollection.Select(Transcribe) as IContentCollectionContainer;
+
+                contentCollection.Children.Clear();
+                contentCollection.Children.AddRange(newCollection.Children);
+            }
+            else if (def is IContent content)
+            {
                 content.Text = CopticInterpreter.IpaTranscribe(content.Text ?? content.SourceText);
-            });
+            }
         }
     }
 }
