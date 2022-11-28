2301d85d3ce6bd33cf0a38b4c5ea9e2e9b626b39
Yoshi Askharoun <jjask7@gmail.com>
2022-11-27 20:15:51 -0600

    Added LanguageInfo class

---
+++ CoptLib/Writing/LanguageInfo.cs
@@ -0,0 +1,92 @@
+﻿using CommunityToolkit.Diagnostics;
+using System;
+using System.Collections.Generic;
+
+namespace CoptLib.Writing;
+
+public class LanguageInfo
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="LanguageInfo"/> using the
+    /// given RFC 5646 language tag.
+    /// </summary>
+    public LanguageInfo(string tag)
+    {
+        Tag = tag;
+
+        string[] subtags = tag.Split('-');
+        Language = subtags[0];
+
+        if (subtags.Length >= 2)
+        {
+            Region = subtags[1];
+
+            if (subtags.Length >= 3)
+                Variant = subtags[2];
+        }
+
+        try
+        {
+            RegionInfo = new(tag);
+        }
+        catch { }
+    }
+
+    /// <summary>
+    /// The RFC 5646 language tag.
+    /// </summary>
+    public string Tag { get; }
+
+    public KnownLanguage? Known { get; private set; }
+
+    public string Language { get; }
+
+    public string Region { get; }
+
+    public string Variant { get; }
+
+    public System.Globalization.RegionInfo RegionInfo { get; }
+
+    /// <summary>
+    /// Parses the given string as a <see cref="KnownLanguage"/> or RFC 5646
+    /// language tag.
+    /// </summary>
+    /// <param name="value">The string to parse.</param>
+    public static LanguageInfo Parse(string value)
+    {
+        Guard.IsNotNull(value);
+
+        // Check if language is known
+        if (Enum.TryParse(value, true, out KnownLanguage kLang)
+            && KnownLanguages.TryGetValue(kLang, out var kLangTag))
+        {
+            return new(kLangTag)
+            {
+                Known = kLang
+            };
+        }
+
+        return new(value);
+    }
+
+    private static readonly IReadOnlyDictionary<KnownLanguage, string> KnownLanguages = new Dictionary<KnownLanguage, string>
+    {
+        [KnownLanguage.Default]	    = "und",
+        [KnownLanguage.Akkadian]	= "akk",
+        [KnownLanguage.Amharic]	    = "am",
+        [KnownLanguage.Arabic]	    = "ar",
+        [KnownLanguage.Aramaic]	    = "arc",
+        [KnownLanguage.Armenian]	= "hy",
+        [KnownLanguage.Coptic]	    = "cop-GR",
+        [KnownLanguage.Dutch]	    = "nl",
+        [KnownLanguage.Egyptian]	= "egy",
+        [KnownLanguage.English]	    = "en",
+        [KnownLanguage.French]	    = "fr",
+        [KnownLanguage.Italian]	    = "it",
+        [KnownLanguage.German]	    = "de",
+        [KnownLanguage.Greek]	    = "el",
+        [KnownLanguage.Hebrew]	    = "he",
+        [KnownLanguage.Latin]	    = "la",
+        [KnownLanguage.Spanish]	    = "es",
+    };
+}
--- CoptTest/Misc.cs
+++ CoptTest/Misc.cs
@@ -1,4 +1,6 @@
 ﻿using CoptLib.IO;
+using CoptLib.Writing;
+using System.Collections.Generic;
 using Xunit;
 
 namespace CoptTest
@@ -12,6 +14,26 @@ namespace CoptTest
             string html = Resource.ReadAllText($"TasbehaOrg_{lyricId}.html");
             var doc = TasbehaOrg.ConvertLyricsPage(html, lyricId);
             string xml = DocWriter.WriteDocXml(doc);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetLanguageInfo_Parse_Samples))]
+        public void LanguageInfo_Parse(string tag)
+        {
+            var li = LanguageInfo.Parse(tag);
+        }
+
+        public static readonly string[] LanguageInfo_Parse_Samples = new string[]
+        {
+            KnownLanguage.English.ToString(), KnownLanguage.Coptic.ToString(),
+            KnownLanguage.Arabic.ToString(), KnownLanguage.Default.ToString(),
+            "cop", "cop-GR", "cop-EG-AST", "de", "it-IT", "en-US"
+        };
+
+        private static IEnumerable<object[]> GetLanguageInfo_Parse_Samples()
+        {
+            foreach (string sample in LanguageInfo_Parse_Samples)
+                yield return new object[] { sample };
         }
     }
 }
