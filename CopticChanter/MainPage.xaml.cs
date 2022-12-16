0e2ceb6a3ae500d8aa3c8e81399ae64f8615bdba
Yoshi Askharoun <jjask7@gmail.com>
2022-12-16 11:33:19 -0600

    Replaced old font conversion UI with loading spinner

--- CopticChanter/MainPage.xaml.cs
+++ CopticChanter/MainPage.xaml.cs
@@ -1,10 +1,8 @@
-﻿using CoptLib;
-using CoptLib.IO;
+﻿using CoptLib.IO;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Windows.Storage;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -24,8 +22,6 @@ namespace CopticChanter
 
         private async void LoadDocs(bool present = false)
         {
-            OutputBox.Text = "";
-            InputBox.Text = "";
             IReadOnlyList<StorageFile> files = await ApplicationData.Current.RoamingFolder.GetFilesAsync();
             try
             {
@@ -68,23 +64,6 @@ namespace CopticChanter
         private void Present()
         {
             Frame.Navigate(typeof(Layouts.DocumentLayout), new Layouts.DocumentLayoutArgs());
-        }
-
-        private async void ConvertButton_Click(object sender, RoutedEventArgs e)
-        {
-			var picker = new Windows.Storage.Pickers.FileOpenPicker
-			{
-				ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
-				SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
-			};
-			picker.FileTypeFilter.Add(".xml");
-            picker.FileTypeFilter.Add(".txt");
-
-            StorageFile file = await picker.PickSingleFileAsync();
-
-            var doc = DocReader.ReadDocXml(file.Path);
-            if (string.IsNullOrWhiteSpace(OutputBox.Text))
-                InputBox.Text += doc.Translations[0];
         }
     }
 }
