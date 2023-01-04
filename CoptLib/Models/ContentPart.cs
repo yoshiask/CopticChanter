87cdc924ed9825356570fbcb0be1e58524f4471c
Yoshi Askharoun <jjask7@gmail.com>
2023-01-03 20:48:45 -0600

    Moved Stanza and Section to separate files

--- CoptLib/Models/ContentPart.cs
+++ CoptLib/Models/ContentPart.cs
@@ -1,9 +1,4 @@
-﻿using CoptLib.IO;
-using CoptLib.Models.Text;
-using CoptLib.Scripting;
-using CoptLib.Writing;
-using System.Collections.Generic;
-using System.Linq;
+﻿using CoptLib.Writing;
 using System.Xml.Serialization;
 
 namespace CoptLib.Models
@@ -44,94 +39,5 @@ namespace CoptLib.Models
         /// all its content, including section headers and stanzas
         /// </summary>
         public virtual int CountRows() => 1;
-    }
-
-    public class Stanza : ContentPart, IContent
-    {
-        private string _sourceText;
-
-        public Stanza(IDefinition parent) : base(parent)
-        {
-
-        }
-
-        public string SourceText
-        {
-            get => _sourceText;
-            set
-            {
-                if (_sourceText != value)
-                {
-                    FontHandled = false;
-                    CommandsHandled = false;
-                    Inlines = new()
-                    {
-                        new Run(value, this)
-                    };
-                }
-
-                _sourceText = value;
-            }
-        }
-
-        public bool CommandsHandled { get; set; }
-
-        public InlineCollection Inlines { get; set; }
-
-        public List<TextCommandBase> Commands { get; set; }
-
-        public void HandleCommands() => ContentHelper.HandleCommands(this);
-
-        public override void HandleFont()
-        {
-            ContentHelper.HandleFont(Inlines);
-            FontHandled = true;
-        }
-
-        public string GetText() => ContentHelper.GetText(this);
-
-        public override string ToString() => GetText();
-    }
-
-    public class Section : ContentPart, IContentCollectionContainer
-    {
-        public Section(IDefinition parent) : base(parent)
-        {
-
-        }
-
-        public IContent Title { get; set; }
-
-        public SimpleContent Source { get; set; }
-
-        public List<ContentPart> Children { get; } = new();
-
-        public override int CountRows()
-        {
-            int count = Children.Sum(p => p.CountRows());
-
-            if (Title != null)
-                count++;
-
-            return count;
-        }
-
-        public void HandleCommands()
-        {
-            DocReader.RecursiveTransform(Children);
-            Title?.HandleCommands();
-        }
-
-        public override void HandleFont()
-        {
-            if (FontHandled)
-                return;
-
-            foreach (ContentPart part in Children)
-                part.HandleFont();
-
-            if (Title != null && Title is IMultilingual multiTitle)
-                multiTitle.HandleFont();
-        }
     }
 }
---
+++ CoptLib/Models/Stanza.cs
@@ -0,0 +1,53 @@
+﻿using CoptLib.Models.Text;
+using CoptLib.Scripting;
+using System.Collections.Generic;
+
+namespace CoptLib.Models
+{
+    public class Stanza : ContentPart, IContent
+    {
+        private string _sourceText;
+
+        public Stanza(IDefinition parent) : base(parent)
+        {
+
+        }
+
+        public string SourceText
+        {
+            get => _sourceText;
+            set
+            {
+                if (_sourceText != value)
+                {
+                    FontHandled = false;
+                    CommandsHandled = false;
+                    Inlines = new()
+                    {
+                        new Run(value, this)
+                    };
+                }
+
+                _sourceText = value;
+            }
+        }
+
+        public bool CommandsHandled { get; set; }
+
+        public InlineCollection Inlines { get; set; }
+
+        public List<TextCommandBase> Commands { get; set; }
+
+        public void HandleCommands() => ContentHelper.HandleCommands(this);
+
+        public override void HandleFont()
+        {
+            ContentHelper.HandleFont(Inlines);
+            FontHandled = true;
+        }
+
+        public string GetText() => ContentHelper.GetText(this);
+
+        public override string ToString() => GetText();
+    }
+}
