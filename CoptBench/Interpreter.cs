10bec9555ed8f20074814be3d7567eb25004c0e0
Yoshi Askharoun <jjask7@gmail.com>
2023-04-18 18:16:28 -0500

    Updated benchmark runner

--- CoptBench/Interpreter.cs
+++ CoptBench/Interpreter.cs
@@ -1,45 +1,37 @@
 ﻿using BenchmarkDotNet.Attributes;
-using CoptLib.Writing;
+using CoptLib.Writing.Linguistics.Analyzers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CoptBench;
 
+[MemoryDiagnoser]
 public class Interpreter
 {
     public IEnumerable<object[]> IpaTranscribe_CopticUnicode_Samples { get; }
+
+    public CopticGrecoBohairicAnalyzer Analyzer { get; }
 
     public Interpreter()
     {
         IpaTranscribe_CopticUnicode_Samples =
             CoptTest.Interpreter.IpaTranscribe_CopticUnicode_Samples
             .Select(p => new object[] { p });
+
+        Analyzer = new();
     }
 
     [Benchmark]
     [ArgumentsSource(nameof(IpaTranscribe_CopticUnicode_Samples))]
     public string IpaTranscribe_CopticUnicode_SingleCached(string sample)
-        => CopticInterpreter.IpaTranscribe(sample);
-
-    [Benchmark]
-    [ArgumentsSource(nameof(IpaTranscribe_CopticUnicode_Samples))]
-    public string IpaTranscribe_CopticUnicode_SingleNoCache(string sample)
-        => CopticInterpreter.IpaTranscribe(sample, false);
+        => Analyzer.IpaTranscribe(sample);
 
     [Benchmark]
     public void IpaTranscribe_CopticUnicode_ManyCached()
     {
         Span<string> samples = CoptTest.Interpreter.IpaTranscribe_CopticUnicode_Samples;
         for (int i = 0; i < samples.Length; i++)
-            CopticInterpreter.IpaTranscribe(samples[i]);
-    }
-
-    [Benchmark]
-    public void IpaTranscribe_CopticUnicode_ManyNoCache()
-    {
-        Span<string> samples = CoptTest.Interpreter.IpaTranscribe_CopticUnicode_Samples;
-        for (int i = 0; i < samples.Length; i++)
-            CopticInterpreter.IpaTranscribe(samples[i], false);
+            Analyzer.IpaTranscribe(samples[i]);
     }
 }
