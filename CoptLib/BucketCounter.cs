95c4fae412a5df559befd489b5ea106044b8721e
Yoshi Askharoun <jjask7@gmail.com>
2023-07-03 22:03:45 -0500

    Add crude language identifier

---
+++ CoptLib/BucketCounter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CoptLib;
+
+public class BucketCounter<TBucket>
+{
+    private int? totalCount = null;
+    private Dictionary<TBucket, int> _dict = new();
+
+    public ICollection<TBucket> Buckets => _dict.Keys;
+
+    public int Total
+    {
+        get => totalCount ?? _dict.Values.Sum();
+        set => totalCount = value < 0 ? null : value;
+    }
+
+    /// <summary>
+    /// Gets or sets the count of the specified <paramref name="bucket"/>.
+    /// </summary>
+    public int this[TBucket bucket]
+    {
+        get
+        {
+            EnsureExists(bucket);
+            return _dict[bucket];
+        }
+        set
+        {
+            EnsureExists(bucket);
+            _dict[bucket] = value;
+        }
+    }
+
+    /// <summary>
+    /// Increments the specified <paramref name="bucket"/>.
+    /// </summary>
+    /// <returns>The new count of the bucket.</returns>
+    public void IncrementBucket(TBucket bucket) => ++this[bucket];
+
+    /// <summary>
+    /// Adds <paramref name="count"/> to the specified <paramref name="bucket"/>.
+    /// </summary>
+    /// <returns>The new count of the bucket.</returns>
+    public int AddToBucket(TBucket bucket, int count) => this[bucket] += count;
+
+    public double GetBucketPercent(TBucket bucket) => (double)this[bucket] / Total;
+
+    public double GetTotalPercent() => (double)_dict.Values.Sum() / Total;
+
+    public TBucket GetLargestBucket() => _dict.Keys.OrderByDescending(b => _dict[b]).FirstOrDefault();
+
+    private bool EnsureExists(TBucket bucket)
+    {
+        if (_dict.ContainsKey(bucket))
+            return false;
+        
+        _dict.Add(bucket, 0);
+        return true;
+    }
+}
--- CoptLib/Writing/Linguistics/LinguisticLanguageService.cs
+++ CoptLib/Writing/Linguistics/LinguisticLanguageService.cs
@@ -1,7 +1,9 @@
 ﻿using CoptLib.Writing.Linguistics.Analyzers;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using AngleSharp.Text;
 
 namespace CoptLib.Writing.Linguistics;
 
@@ -56,5 +58,55 @@ public class LinguisticLanguageService
         }
 
         return factory(languageInfo);
+    }
+
+    /// <summary>
+    /// Attempts to identify what language a piece of text is written in.
+    /// </summary>
+    /// <param name="text">The text to analyze.</param>
+    /// <param name="language">The identified language, if any.</param>
+    /// <returns>Whether a language was identified.</returns>
+    public static bool TryIdentifyLanguage(string text, out LanguageInfo language)
+    {
+        BucketCounter<KnownLanguage> languageCounts = new();
+        
+        foreach (var c in text)
+        {
+            if ((int)CharUnicodeInfo.GetUnicodeCategory(c) > 10 || c.IsWhiteSpaceCharacter())
+                continue;
+
+            ++languageCounts.Total;
+
+            KnownLanguage charLang = c switch
+            {
+                >= 'A' and <= 'z' => KnownLanguage.English,
+                >= 'Ϣ' and <= 'ϯ' or >= 'Ⲁ' and <= 'ⲱ' => KnownLanguage.Coptic,
+                >= '\u0600' and <= '\u08FF' or >= '\uFB50' and <= '\uFEFC' => KnownLanguage.Arabic,
+                >= 'Ά' and <= 'ώ' => KnownLanguage.Greek,
+                _ => KnownLanguage.Default
+            };
+
+            ++languageCounts[charLang];
+        }
+
+        if (languageCounts.GetTotalPercent() < 0.10)
+        {
+            language = null;
+            return false;
+        }
+
+        var knownLanguage = languageCounts.GetLargestBucket();
+        if (knownLanguage == KnownLanguage.Greek)
+        {
+            // Make sure it really is Greek, and not just Coptic written
+            // using the Greek Unicode section.
+            var copticPercent = languageCounts.GetBucketPercent(KnownLanguage.Coptic);
+            var greekPercent = languageCounts.GetBucketPercent(KnownLanguage.Greek);
+            if (copticPercent >= 0.80 || (copticPercent > 0 && greekPercent >= 0.80))
+                knownLanguage = KnownLanguage.Coptic;
+        }
+
+        language = new(knownLanguage);
+        return true;
     }
 }
