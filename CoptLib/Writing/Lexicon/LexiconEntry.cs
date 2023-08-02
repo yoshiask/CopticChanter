d2bad9db0cdacb04d53a025c7b685cdc9b8ab48e
Yoshi Askharoun <jjask7@gmail.com>
2023-08-01 22:22:38 -0500

    Create basic lexicon interfaces

---
+++ CoptLib/Writing/Lexicon/LexiconEntry.cs
@@ -0,0 +1,40 @@
+﻿using CoptLib.Models;
+using System.Collections.Generic;
+
+namespace CoptLib.Writing.Lexicon;
+
+public interface ILexiconEntry
+{
+}
+
+public record LexiconSuperEntry(List<LexiconEntry> Entries) : ILexiconEntry;
+
+public record LexiconEntry(EntryType Type, List<Form> Forms, List<Sense> Senses, GrammarGroup GrammarGroup) : ILexiconEntry;
+
+public enum EntryType : byte
+{
+    Hom,
+    Compound,
+    Foreign,
+}
+
+/// <summary>
+/// Represents a specific form of a lexicon entry.
+/// </summary>
+/// <param name="Type"></param>
+/// <param name="Usage">The language or dialect that uses this form.</param>
+/// <param name="Orthography">The orthography of this form.</param>
+public record Form(FormType Type, LanguageInfo Usage, string Orthography)
+{
+}
+
+public enum FormType : byte
+{
+    Lemma,
+    Inflected,
+    Compound,
+}
+
+public record Sense(TranslationCollection Translations, string Bibliography)
+{
+}
