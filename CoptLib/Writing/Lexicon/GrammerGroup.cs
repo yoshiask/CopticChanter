6fdf92c8e1421148d20aee77b4834e77a0c242a2
Yoshi Askharoun <jjask7@gmail.com>
2023-08-01 22:22:38 -0500

    Create basic lexicon interfaces

---
+++ CoptLib/Writing/Lexicon/GrammerGroup.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+using System.Collections.Generic;
+
+namespace CoptLib.Writing.Lexicon;
+
+public record GrammarGroup(PartOfSpeech PartOfSpeech, Number Number, Gender? Gender,
+    List<GrammarEntry>? Entries, string? Subclass, string? Note);
+
+public record GrammarEntry(GrammarType Type, string Text);
+
+public enum PartOfSpeech : byte
+{
+    Subject,    // Subst.
+    Verb,       //Vb.
+    Adjective,  //Adj.
+    Adverb,     //Adv.
+    Preposition,//Präp.
+    Zahlzeichen,//???
+    NominalPrefix,//Nominalpräfix
+    PossessiveArticle,//Possessivartikel
+}
+
+public enum Number : byte
+{
+    None,
+    Singular,   //sg.
+    Plural,     //pl.
+}
+
+public enum Gender : byte
+{
+    Neutral, Masculine, Feminine
+}
+
+public enum GrammarType : byte
+{
+    CollocPrep,
+    CollocAdv,
+    CollocParticle,
+    CollocNoun,
+}
---
+++ CoptLib/Writing/Lexicon/ILexicon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoptLib.Writing.Lexicon;
+
+/// <summary>
+/// An interface that represents a lexicon.
+/// </summary>
+public interface ILexicon
+{
+    
+}
