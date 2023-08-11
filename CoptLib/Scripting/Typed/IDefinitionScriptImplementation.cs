698ca34b1286c16669173c48fde09dfab28d749d
Yoshi Askharoun <jjask7@gmail.com>
2023-08-10 23:13:45 -0700

    Split DotNetDefinitionScript into own file

--- CoptLib/Scripting/Typed/IDefinitionScriptImplementation.cs
+++ CoptLib/Scripting/Typed/IDefinitionScriptImplementation.cs
@@ -17,18 +17,3 @@ public abstract class DefinitionScriptImplementationBase : IDefinitionScriptImpl
 
     public abstract IDefinition Execute(LoadContextBase? context);
 }
-
-public class DotNetDefinitionScript : DotNetScript<DefinitionScriptImplementationBase, IDefinition>
-{
-    public DotNetDefinitionScript(string scriptBody, IDefinition? parent = null) : base(scriptBody, parent)
-    {
-    }
-
-    protected override IDefinition ExecuteInternal(LoadContextBase? context)
-    {
-        var output = Implementation!.Execute(DocContext?.Context);
-        output.DocContext = DocContext;
-        output.Parent = this;
-        return output;
-    }
-}
