952aee62c41b05cb20603d08af050316036f52bb
Yoshi Askharoun <jjask7@gmail.com>
2023-08-10 23:11:45 -0700

    Add auto-keyed dictionary

---
+++ CoptLib/ElementKeyedDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoptLib;
+
+/// <summary>
+/// A <see cref="SortedDictionary{TKey,TValue}"/>
+/// </summary>
+/// <typeparam name="TKey">
+/// The type of the keys in the dictionary.
+/// </typeparam>
+/// <typeparam name="TValue">
+/// The type of the values in the dictionary.
+/// </typeparam>
+public class ElementKeyedDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+{
+    public ElementKeyedDictionary(Func<TValue, TKey> keySelector)
+    {
+        KeySelector = keySelector;
+    }
+
+    /// <summary>
+    /// Gets the key given the entire value.
+    /// </summary>
+    public Func<TValue, TKey> KeySelector { get; }
+
+    /// <summary>
+    /// Adds the specified key and value to the dictionary.
+    /// </summary>
+    /// <param name="value">
+    /// The value of the element to add.
+    /// </param>
+    public void Add(TValue value) => Add(KeySelector(value), value);
+}
