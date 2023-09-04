b6e976231ae7d12d2a199ae5a7cb0ea3cc5959ca
Yoshi Askharoun <jjask7@gmail.com>
2023-09-04 12:28:24 -0500

    Add helper method for enumerating Docs in a Sequence

--- CoptLib/Models/Sequences/Sequence.cs
+++ CoptLib/Models/Sequences/Sequence.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using CoptLib.IO;
 
@@ -14,19 +15,19 @@ public class Sequence : IContextualLoad
         Key = key;
         Nodes = new(node => node.Id);
     }
-    
+
     public string? Key { get; set; }
-    
+
     public string? Name { get; set; }
-    
+
     [NotNull]
     public LoadContextBase? Context { get; set; }
-    
+
     /// <summary>
     /// The identifier of the node to always start at.
     /// </summary>
     public int RootNodeId { get; }
-    
+
     /// <summary>
     /// A collection of all nodes in the graph.
     /// </summary>
@@ -36,9 +37,16 @@ public class Sequence : IContextualLoad
     /// Creates an <see cref="IAsyncEnumerable{T}"/> that steps through
     /// the sequence in order.
     /// </summary>
-    /// <returns></returns>
     public IAsyncEnumerable<SequenceNode> EnumerateNodes()
         => new SequenceEnumerable(Nodes[RootNodeId], Context, ResolveNodeAsync);
+
+    /// <summary>
+    /// Creates an <see cref="IAsyncEnumerable{T}"/> that steps through
+    /// documents in the sequence in order.
+    /// </summary>
+    public virtual IAsyncEnumerable<Doc> EnumerateDocs() => EnumerateNodes()
+        .Select(n => Context.LookupDefinition(n.DocumentKey))
+        .OfType<Doc>();
 
     protected virtual Task<SequenceNode> ResolveNodeAsync(int nodeId) => Task.FromResult(Nodes[nodeId]);
 }
