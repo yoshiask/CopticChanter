61a80a365493f5e098a7e16ee75aa6565b222b87
Yoshi Askharoun <jjask7@gmail.com>
2023-09-06 22:31:30 -0500

    Add DocLayout test with transliteration

--- CoptTest/DocOps.cs
+++ CoptTest/DocOps.cs
@@ -1,6 +1,7 @@
 ﻿using CoptLib.IO;
 using CoptLib.Models;
 using CoptLib.ViewModels;
+using CoptLib.Writing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,8 +23,32 @@ public class DocOps
         var table = await dvm.CreateTableAsync();
         Assert.Equal(rowCountEx, table.Count);
 
-        static void docTitleCheck(List<object> row) => Assert.Single(row);
-        void rowCheck(List<object> row) => Assert.Equal(colCountEx, row.Count);
+        static void docTitleCheck(List<IDefinition> row) => Assert.Single(row);
+        void rowCheck(List<IDefinition> row) => Assert.Equal(colCountEx, row.Count);
+
+        var tableCheckActions = Enumerable.Repeat(rowCheck, rowCountEx - 1).Prepend(docTitleCheck);
+        Assert.Collection(table, tableCheckActions.ToArray());
+    }
+
+    [Theory]
+    [InlineData("The Morning Doxology.xml", 74, 2)]
+    public async Task DocLayout_GenerateWithTransliteration(string file, int rowCountEx, int colCountEx)
+    {
+        string xml = Resource.ReadAllText(file);
+        Doc doc = DocReader.ParseDocXml(XDocument.Parse(xml));
+
+        List<LanguageInfo> transliterationTargets = new() { LanguageInfo.Parse("cop/en") };
+        DocLayoutOptions layoutOptions = new()
+        {
+            Transliterations = transliterationTargets
+        };
+        DocViewModel dvm = new(new DocLayout(doc, layoutOptions));
+
+        var table = await dvm.CreateTableAsync();
+        Assert.Equal(rowCountEx, table.Count);
+
+        static void docTitleCheck(List<IDefinition> row) => Assert.Single(row);
+        void rowCheck(List<IDefinition> row) => Assert.Equal(colCountEx + transliterationTargets.Count, row.Count);
 
         var tableCheckActions = Enumerable.Repeat(rowCheck, rowCountEx - 1).Prepend(docTitleCheck);
         Assert.Collection(table, tableCheckActions.ToArray());
