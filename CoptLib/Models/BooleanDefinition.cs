e0ddcb1f1640665fb657357c790b5f99548191c1
Yoshi Askharoun <jjask7@gmail.com>
2024-04-16 23:35:37 -0500

    Add bool command and definition wrapper

---
+++ CoptLib/Models/BooleanDefinition.cs
@@ -0,0 +1,12 @@
+﻿namespace CoptLib.Models;
+
+public class BooleanDefinition : Definition
+{
+    public BooleanDefinition(bool value, IDefinition? parent)
+    {
+        Value = value;
+        Parent = parent;
+    }
+    
+    public bool Value { get; }
+}
---
+++ CoptLib/Scripting/Commands/BooleanCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CoptLib.Extensions;
+using CoptLib.IO;
+using CoptLib.Models;
+using CoptLib.Models.Text;
+
+namespace CoptLib.Scripting.Commands;
+
+public class BooleanCommand(string name, InlineCommand inline, IDefinition[] parameters)
+    : TextCommandBase(name, inline, parameters), ICommandOutput<bool>
+{
+    protected override IDefinition? ExecuteInternal(ILoadContext? context)
+    {
+        var value = !Name.Equals("false", StringComparison.InvariantCultureIgnoreCase);
+        var key = value ? "True" : "False";
+
+        // Attempt to reference existing definition instead of eating memory for a single bool
+        if (context?.TryLookupDefinition(key, out var def) ?? false)
+        {
+            def.RegisterReference(Inline);
+            return def;
+        }
+        return new BooleanDefinition(value, Inline);
+    }
+
+    bool ICommandOutput<bool>.Output => (Output as BooleanDefinition)?.Value ?? false;
+
+    bool ICommandOutput<bool>.Execute(ILoadContext? context)
+    {
+        Execute(context);
+        return ((ICommandOutput<bool>)this).Output;
+    }
+}
