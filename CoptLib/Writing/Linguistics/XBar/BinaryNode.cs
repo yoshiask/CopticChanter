dbf6d3972bfb92b747a3d09f0a6a11415e5a3135
Yoshi Askharoun <jjask7@gmail.com>
2024-05-28 19:26:03 -0500

    Add SimpleBitstream and BinaryNode, in XBar namespace

---
+++ CoptLib/Writing/Linguistics/XBar/BinaryNode.cs
@@ -0,0 +1,149 @@
+﻿using CoptLib.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoptLib.Writing.Linguistics.XBar;
+
+public class BinaryNode<T>
+{
+    public BinaryNode<T>? Parent { get; set; }
+
+    public BinaryNode<T>? Left { get; set; }
+
+    public BinaryNode<T>? Right { get; set; }
+
+    public T? Value { get; set; }
+
+    public BinaryNode(T? value = default, BinaryNode<T>? left = null, BinaryNode<T>? right = null, BinaryNode<T>? parent = null)
+    {
+        Parent = parent;
+        Value = value;
+
+        if (left is not null)
+        {
+            left.Parent = this;
+            Left = left;
+        }
+
+        if (right is not null)
+        {
+            right.Parent = this;
+            Right = right;
+        }
+    }
+
+    /// <summary>
+    /// Traverses the provided path by moving left and right as instructed by the bitstream.
+    /// </summary>
+    /// <param name="path">
+    /// The path to follow, where <see langword="true"/> is interpreted as right, and <see langword="false"/> as left.
+    /// </param>
+    /// <returns>The descendant node once the bitstream is exhausted.</returns>
+    /// <exception cref="IndexOutOfRangeException">The bitstream was longer than the depth of the tree.</exception>
+    public BinaryNode<T> Traverse(ISimpleBitstream path)
+    {
+        BinaryNode<T>? node = this;
+        foreach (var takeRight in path)
+        {
+            node = takeRight ? node?.Right : node?.Left;
+
+            if (node is null)
+                throw new IndexOutOfRangeException();
+        }
+
+        return node;
+    }
+
+    public IEnumerable<BinaryNode<T>> EnumeratePreOrder()
+    {
+        yield return this;
+
+        if (Left is not null)
+            foreach (var leftDescendantNode in Left.EnumeratePostOrder())
+                yield return leftDescendantNode;
+            
+        if (Right is not null)
+            foreach (var rightDescendantNode in Right.EnumeratePostOrder())
+                yield return rightDescendantNode;
+    }
+
+    public IEnumerable<BinaryNode<T>> EnumerateInOrder()
+    {
+        if (Left is not null)
+            foreach (var leftDescendantNode in Left.EnumerateInOrder())
+                yield return leftDescendantNode;
+
+        yield return this;
+            
+        if (Right is not null)
+            foreach (var rightDescendantNode in Right.EnumerateInOrder())
+                yield return rightDescendantNode;
+    }
+
+    public IEnumerable<BinaryNode<T>> EnumeratePostOrder()
+    {
+        if (Left is not null)
+            foreach (var leftDescendantNode in Left.EnumeratePostOrder())
+                yield return leftDescendantNode;
+
+        if (Right is not null)
+            foreach (var rightDescendantNode in Right.EnumeratePostOrder())
+                yield return rightDescendantNode;
+
+        yield return this;
+    }
+
+    public IEnumerable<BinaryNode<T>> EnumerateLevelOrder()
+    {
+        Queue<BinaryNode<T>> queue = new();
+        queue.Enqueue(this);
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            yield return node;
+
+            if (node.Left != null)
+                queue.Enqueue(node.Left);
+            
+            if (node.Right != null)
+                queue.Enqueue(node.Right);
+        }
+    }
+
+    /// <summary>
+    /// Represents the binary tree using Graphviz's DOT language.
+    /// </summary>
+    /// <param name="sb">A <see cref="StringBuilder"/> to write to.</param>
+    public StringBuilder SerializeToDot(StringBuilder sb)
+    {
+        sb.AppendLine("strict graph {");
+        sb.AppendLine("    rankdir=\"BT\"");
+        sb.AppendLine();
+
+        var nodes = EnumerateLevelOrder().ToList();
+
+        sb.AppendLine($"    n{nodes[0].GetHashCode()} [label=\"{nodes[0].Value}\"]");
+
+        foreach (var node in nodes)
+        {
+            if (node.Parent is null)
+                continue;
+
+            var id = node.GetHashCode();
+            sb.AppendLine($"    n{id} [label=\"{node.Value}\"]");
+            sb.AppendLine($"    n{id} -- n{node.Parent.GetHashCode()}");
+        }
+
+        sb.AppendLine("}");
+
+        return sb;
+    }
+
+    /// <summary>
+    /// Represents the binary tree using Graphviz's DOT language.
+    /// </summary>
+    public string SerializeToDot() => SerializeToDot(new StringBuilder()).ToString();
+}
---
+++ CoptLib/Writing/Linguistics/XBar/StructuralWord.cs
@@ -0,0 +1,35 @@
+﻿using CoptLib.Writing.Lexicon;
+using System.Collections.Generic;
+
+namespace CoptLib.Writing.Linguistics.XBar;
+
+public interface IStructuralElement
+{
+    string SourceText { get; }
+
+    IDictionary<string, object> Attributes { get; }
+}
+
+public abstract record StructuralElement(string SourceText) : IStructuralElement
+{
+    public IDictionary<string, object> Attributes { get; } = new Dictionary<string, object>();
+}
+
+public record StructuralLexeme(string SourceText, LexiconEntry Entry, int SenseIndex) : StructuralElement(SourceText)
+{
+    public Sense Sense => Entry.Senses[SenseIndex];
+}
+
+public record StructuralWord(string SourceText, IList<StructuralLexeme> Lexemes) : StructuralElement(SourceText)
+{
+    public StructuralWord(string sourceText) : this(sourceText, [])
+    {
+    }
+}
+
+public record StructuralSentence(string SourceText, IList<StructuralWord> Words) : StructuralElement(SourceText)
+{
+    public StructuralSentence(string sourceText) : this(sourceText, [])
+    {
+    }
+}
