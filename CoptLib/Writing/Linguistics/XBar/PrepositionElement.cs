04fa121d233365a19c301c77ea3dd12dd4b28e67
Yoshi Askharoun <jjask7@gmail.com>
2024-10-03 15:25:47 -0500

    Add prepositions

---
+++ CoptLib/Writing/Linguistics/XBar/PrepositionElement.cs
@@ -0,0 +1,38 @@
+﻿namespace CoptLib.Writing.Linguistics.XBar;
+
+public record PrepositionElement(Range SourceRange, PrepositionMeta Meta) : StructuralElement(SourceRange);
+
+public record PrepositionMeta(PrepositionType Type, bool Negative = false);
+
+public enum PrepositionType
+{
+    Unspecified,
+    To,
+    From,
+    Above,
+    Between,
+    Beside,
+    With,
+    In,
+    On,
+    Under,
+    Away,
+    Ago,
+    Along,
+    Through,
+    Since,
+    For,
+    Of,
+    BecauseOf,
+    AccordingTo,
+    After,
+    OtherSide,
+    Except,
+    Contrasting,
+    Forward,
+    Behind,
+    Concerning,
+    Facing,
+    PresenceOf,
+    Beyond,
+}
