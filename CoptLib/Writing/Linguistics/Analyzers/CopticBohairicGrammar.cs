9a0c9bae42ca1ae3f423e6c9ba5d16f6ed9b3c18
Yoshi Askharoun <jjask7@gmail.com>
2024-11-02 18:10:26 -0500

    Move most Bohairic grammar rules to separate class

---
+++ CoptLib/Writing/Linguistics/Analyzers/CopticBohairicGrammar.cs
@@ -0,0 +1,160 @@
+﻿using CoptLib.Writing.Lexicon;
+using CoptLib.Writing.Linguistics.XBar;
+using System;
+using System.Collections.Generic;
+
+namespace CoptLib.Writing.Linguistics.Analyzers;
+
+public class CopticBohairicGrammar
+{
+    private IEnumerable<SemanticPair>? _determiners;
+    private IEnumerable<SemanticPair>? _nounPrefixes;
+
+    public IEnumerable<SemanticPair> Articles { get; } =
+    [
+        // Definite
+        new("ⲡ", () => new DeterminerArticleMeta(DeterminerStrength.Weak, true, new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲫ", () => new DeterminerArticleMeta(DeterminerStrength.Weak, true, new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲡⲓ", () => new DeterminerArticleMeta(DeterminerStrength.Strong, true, new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧ", () => new DeterminerArticleMeta(DeterminerStrength.Weak, true, new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲑ", () => new DeterminerArticleMeta(DeterminerStrength.Weak, true, new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ϯ", () => new DeterminerArticleMeta(DeterminerStrength.Strong, true, new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲓ", () => new DeterminerArticleMeta(DeterminerStrength.Strong, true, new(Number: GrammaticalCount.Plural))),
+
+        // Indefinite
+        new("ⲟⲩ", () => new DeterminerArticleMeta(default, false, new(Number: GrammaticalCount.Singular))),
+        new("ϩⲁⲛ", () => new DeterminerArticleMeta(default, false, new(Number: GrammaticalCount.Plural))),
+
+        // Possessive Strong
+        new("ⲛ", () => new DeterminerPossessiveMeta(DeterminerStrength.Strong, InflectionMeta.Unspecified, InflectionMeta.Unspecified)),
+        new("ⲙ", () => new DeterminerPossessiveMeta(DeterminerStrength.Strong, InflectionMeta.Unspecified, InflectionMeta.Unspecified)),
+
+        // Possessive 1st Person
+        new("ⲡⲁ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Singular, PointOfView.First), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲁ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Singular, PointOfView.First), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲁ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Singular, PointOfView.First), new(Number: GrammaticalCount.Plural))),
+        new("ⲡⲉⲛ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.First), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲉⲛ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.First), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲉⲛ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.First), new(Number: GrammaticalCount.Plural))),
+
+        // Possessive 2nd Person
+        new("ⲡⲉⲕ", () => new DeterminerPossessiveMeta(default, new(Gender.Masculine, GrammaticalCount.Singular, PointOfView.Second), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲉⲕ", () => new DeterminerPossessiveMeta(default, new(Gender.Masculine, GrammaticalCount.Singular, PointOfView.Second), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲉⲕ", () => new DeterminerPossessiveMeta(default, new(Gender.Masculine, GrammaticalCount.Singular, PointOfView.Second), new(Number: GrammaticalCount.Plural))),
+        new("ⲡⲉ", () => new DeterminerPossessiveMeta(default, new(Gender.Feminine, GrammaticalCount.Singular, PointOfView.Second), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲉ", () => new DeterminerPossessiveMeta(default, new(Gender.Feminine, GrammaticalCount.Singular, PointOfView.Second), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲉ", () => new DeterminerPossessiveMeta(default, new(Gender.Feminine, GrammaticalCount.Singular, PointOfView.Second), new(Number: GrammaticalCount.Plural))),
+        new("ⲡⲉⲧⲉⲛ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.Second), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲉⲧⲉⲛ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.Second), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲉⲧⲉⲛ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.Second), new(Number: GrammaticalCount.Plural))),
+
+        // Possessive 3rd Person
+        new("ⲡⲉϥ", () => new DeterminerPossessiveMeta(default, new(Gender.Masculine, GrammaticalCount.Singular, PointOfView.Third), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲉϥ", () => new DeterminerPossessiveMeta(default, new(Gender.Masculine, GrammaticalCount.Singular, PointOfView.Third), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲉϥ", () => new DeterminerPossessiveMeta(default, new(Gender.Masculine, GrammaticalCount.Singular, PointOfView.Third), new(Number: GrammaticalCount.Plural))),
+        new("ⲡⲉⲥ", () => new DeterminerPossessiveMeta(default, new(Gender.Feminine, GrammaticalCount.Singular, PointOfView.Third), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲉⲥ", () => new DeterminerPossessiveMeta(default, new(Gender.Feminine, GrammaticalCount.Singular, PointOfView.Third), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲉⲥ", () => new DeterminerPossessiveMeta(default, new(Gender.Feminine, GrammaticalCount.Singular, PointOfView.Third), new(Number: GrammaticalCount.Plural))),
+        new("ⲡⲟⲩ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.Third), new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲟⲩ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.Third), new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲟⲩ", () => new DeterminerPossessiveMeta(default, new(default, GrammaticalCount.Plural, PointOfView.Third), new(Number: GrammaticalCount.Plural))),
+
+        // Demonstrative
+        new("ⲡⲁⲓ", () => new DeterminerDemonstrativeMeta(DeterminerStrength.Near, new(Gender.Masculine, GrammaticalCount.Singular))),
+        new("ⲧⲁⲓ", () => new DeterminerDemonstrativeMeta(DeterminerStrength.Near, new(Gender.Feminine, GrammaticalCount.Singular))),
+        new("ⲛⲁⲓ", () => new DeterminerDemonstrativeMeta(DeterminerStrength.Near, new(Number: GrammaticalCount.Plural))),
+    ];
+
+    public IEnumerable<SemanticPair> ComplexPrefixes { get; } =
+    [
+        // Example: "ⲡⲓⲙⲁⲛϣⲉⲗⲉⲧ" / "the bridal chamber" ("the place of [the] bride")
+        new("ⲙⲁ", () => new NounMeta(new("/c/en/house"), InflectionMeta.Unspecified)),
+
+        // Example: "ϯⲙⲉⲧⲣⲉⲙⲛ̀ⲭⲏⲙⲓ" / "the Coptic language" ("[the language of] person of Egypt")
+        new("ⲣⲉⲙ", () => new NounMeta(new("/c/en/person"), InflectionMeta.Unspecified)),
+
+        // Example: "ⲟⲩⲣⲉϥⲉⲣⲛⲟⲃⲓ" / "a sinner" ("a person that sins")
+        new("ⲣⲉϥ", () => new CompoundMeta([new NounMeta(new("/c/en/person"), InflectionMeta.Unspecified),])),
+    ];
+
+    public IEnumerable<SemanticPair> Determiners
+    {
+        get
+        {
+            return _determiners ??= [
+                new("ⲉⲧⲉⲙⲙⲁⲩ", () => new DeterminerDemonstrativeMeta(DeterminerStrength.Far, InflectionMeta.Unspecified)),
+                new("ⲛⲓⲃⲉⲛ", () => new DeterminerQuantifyingMeta(new(Number: GrammaticalCount.All))),
+                .. Articles
+            ];
+        }
+    }
+
+    public IEnumerable<SemanticPair> NounPrefixes
+    {
+        get
+        {
+            return _nounPrefixes ??= [
+                new("ⲉ", () => new PrepositionMeta(PrepositionType.To)),
+                //["ⲉⲟⲩ"] = null,
+                //["ⲉⲩ"] = null,    // Contraction of ⲉⲟⲩ
+
+                new("ⲛ", () => new PrepositionMeta(PrepositionType.Of)),
+                new("ⲙ", () => new PrepositionMeta(PrepositionType.Of)),
+
+                .. Articles
+            ];
+        }
+    }
+
+    public IEnumerable<SemanticPair> Prepositions { get; } =
+    [
+        new("ⲛⲧⲉ", () => new PrepositionMeta(PrepositionType.Of)),
+        new("ϩⲓⲧⲉⲛ", () => new PrepositionMeta(PrepositionType.Through)),
+
+        new("ⲁⲧϭⲛⲉ", () => new PrepositionMeta(PrepositionType.With, true)),
+        new("ⲥⲁⲃⲟⲗ", () => new PrepositionMeta(PrepositionType.Away)),
+        new("ϣⲁⲉⲃⲟⲗ", () => new PrepositionMeta(PrepositionType.In, true)),
+        new("ⲉⲑⲃⲉ", () => new PrepositionMeta(PrepositionType.BecauseOf)),
+        new("ⲓⲥϫⲉⲛ", () => new PrepositionMeta(PrepositionType.Since)),
+        new("ⲕⲁⲧⲁ", () => new PrepositionMeta(PrepositionType.AccordingTo)),
+        new("ⲗⲟⲓⲡⲟⲛ", () => new PrepositionMeta(PrepositionType.After)),
+        new("ⲙⲉⲛϩⲓ", () => new PrepositionMeta(PrepositionType.After)),
+        new("ⲙⲏⲣ", () => new PrepositionMeta(PrepositionType.OtherSide)),
+        new("ϩⲓⲙⲏⲣ", () => new PrepositionMeta(PrepositionType.OtherSide)),
+        new("ⲉⲧⲙⲏϯ", () => new PrepositionMeta(PrepositionType.Between)),
+        //new("ⲙⲙⲟⲛ", () => new PrepositionMeta(PrepositionType.X)),
+        new("ⲛⲉⲙ", () => new PrepositionMeta(PrepositionType.With)),
+        new("ⲡⲗⲏⲛ", () => new PrepositionMeta(PrepositionType.Except)),
+        new("ⲙⲉⲛⲉⲛⲥⲁ", () => new PrepositionMeta(PrepositionType.After)),
+        new("ⲉⲥⲕⲉⲛ", () => new PrepositionMeta(PrepositionType.Beside)),
+        new("ⲛⲧⲉⲛ", () => new PrepositionMeta(PrepositionType.BecauseOf)),
+        new("ϧⲁⲧⲉⲛ", () => new PrepositionMeta(PrepositionType.Under)),
+        new("ⲟⲩⲃⲉ", () => new PrepositionMeta(PrepositionType.Contrasting)),
+        new("ⲟⲩⲧⲉ", () => new PrepositionMeta(PrepositionType.Between)),
+        //new("ⲛⲟⲩⲉϣⲉⲛ", () => new PrepositionMeta(PrepositionType.X)),
+        new("ⲉⲫⲁϩⲟⲩ", () => new PrepositionMeta(PrepositionType.Forward, true)),
+        new("ⲥⲁⲫⲁϩⲟⲩ", () => new PrepositionMeta(PrepositionType.Behind)),
+        new("ϩⲓⲫⲁϩⲟⲩ", () => new PrepositionMeta(PrepositionType.Behind)),
+        new("ⲭⲱⲣⲓⲥ", () => new PrepositionMeta(PrepositionType.With, true)),
+        new("ϣⲁ", () => new PrepositionMeta(PrepositionType.To)),
+        new("ϧⲁ", () => new PrepositionMeta(PrepositionType.In)),
+        //new("ϣⲁⲧⲉⲛ", () => new PrepositionMeta(PrepositionType.To)),
+        new("ϧⲉⲛ", () => new PrepositionMeta(PrepositionType.In)),
+        new("ϩⲁ", () => new PrepositionMeta(PrepositionType.To)),
+        new("ⲉⲑⲏ", () => new PrepositionMeta(PrepositionType.Forward)),
+        new("ⲥⲁⲧϩⲏ", () => new PrepositionMeta(PrepositionType.After, true)),
+        new("ϩⲁⲑⲏ", () => new PrepositionMeta(PrepositionType.Behind, true)),
+        new("ϩⲓⲑⲏ", () => new PrepositionMeta(PrepositionType.Forward)),
+        new("ϩⲓ", () => new PrepositionMeta(PrepositionType.Concerning)),
+        new("ⲉϩⲣⲉⲛ", () => new PrepositionMeta(PrepositionType.Facing)),
+        new("ⲛⲁϩⲣⲉⲛ", () => new PrepositionMeta(PrepositionType.PresenceOf)),
+        new("ⲉϧⲟⲩⲛ", () => new PrepositionMeta(PrepositionType.To)),
+        new("ϩⲟⲧⲉ", () => new PrepositionMeta(PrepositionType.Beyond)),
+        new("ⲉϩⲣⲏⲓ", () => new PrepositionMeta(PrepositionType.Above)),
+        new("ⲉϫⲉⲛ", () => new PrepositionMeta(PrepositionType.On)),
+        new("ϧⲁϫⲉⲛ", () => new PrepositionMeta(PrepositionType.After, true)),
+        new("ϩⲓϫⲉⲛ", () => new PrepositionMeta(PrepositionType.On)),
+    ];
+}
+
+public record SemanticPair(Pattern Pattern, Func<IMeta> MetaFactory);
---
+++ CoptLib/Writing/Pattern.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CoptLib.Writing;
+
+/// <summary>
+/// An interface for basic searches within strings.
+/// </summary>
+/// <remarks>
+/// Functionally, a port of Dart's <see href="https://api.dart.dev/stable/3.5.4/dart-core/Pattern-class.html">Pattern class</see>.
+/// </remarks>
+public abstract class Pattern
+{
+    public abstract IEnumerable<PatternMatch> AllMatches(string str, int start = 0);
+
+    public abstract PatternMatch? MatchAsPrefix(string str, int start = 0);
+
+    public static implicit operator Pattern(Regex regex) => new RegexPattern(regex);
+    public static implicit operator Pattern(string expression) => new StringPattern(expression);
+}
+
+public sealed class RegexPattern(Regex regex) : Pattern
+{
+    public Regex RegularExpression { get; } = regex;
+
+    public override IEnumerable<PatternMatch> AllMatches(string str, int start = 0)
+    {
+        return RegularExpression.Matches(str, start)
+            .Cast<Match>()
+            .Where(m => m.Success)
+            .Select(m => CreateMatch(m, str));
+    }
+
+    public override PatternMatch? MatchAsPrefix(string str, int start = 0)
+    {
+        var match = RegularExpression.Match(str, start);
+
+        return match.Success && match.Index == 0
+            ? CreateMatch(match, str)
+            : null;
+    }
+
+    public static implicit operator RegexPattern(Regex regex) => new(regex);
+    public static implicit operator RegexPattern(string regex) => new(new Regex(regex));
+
+    private PatternMatch CreateMatch(Match reMatch, string str)
+    {
+        Dictionary<int, string?> groups = new(reMatch.Groups.Count);
+        for (int i = 0; i < reMatch.Groups.Count; i++)
+        {
+            var group = reMatch.Groups[i];
+            groups[i] = group.Success ? group.Value : null;
+        }
+
+        return new PatternMatch
+        {
+            Start = reMatch.Index,
+            End = reMatch.Index + reMatch.Length,
+            Input = str,
+            Pattern = this,
+            Groups = groups,
+        };
+    }
+}
+
+public sealed class StringPattern(string expression) : Pattern
+{
+    public string Expression { get; } = expression;
+
+    public override IEnumerable<PatternMatch> AllMatches(string str, int start = 0)
+    {
+        int searchIndex = start;
+        while (searchIndex < str.Length)
+        {
+            searchIndex = str.IndexOf(Expression, searchIndex);
+            if (searchIndex < 0)
+                break;
+
+            yield return new PatternMatch
+            {
+                Start = searchIndex,
+                End = searchIndex + Expression.Length,
+                Input = str,
+                Pattern = this,
+                Groups = [],
+            };
+        }
+    }
+
+    public override PatternMatch? MatchAsPrefix(string str, int start = 0)
+    {
+        var matchIndex = str.IndexOf(Expression, start);
+
+        if (matchIndex != start)
+            return null;
+
+        return new PatternMatch
+        {
+            Start = matchIndex,
+            End = matchIndex + Expression.Length,
+            Input = str,
+            Pattern = this,
+            Groups = [],
+        };
+    }
+
+    public static implicit operator StringPattern(string expression) => new(expression);
+}
+
+public sealed class PatternMatch
+{
+    public int Start { get; init; }
+    public int End { get; init; }
+    public required string Input { get; init; }
+    public required Pattern Pattern { get; init; }
+    public required Dictionary<int, string?> Groups { get; init; }
+}
