54e6a8650aa68c8047b4c1332fda6fab9e2c82e8
Yoshi Askharoun <jjask7@gmail.com>
2024-11-03 23:34:40 -0600

    Support sub-grammar groups

--- CoptLib/Writing/Lexicon/TeiLexicon.cs
+++ CoptLib/Writing/Lexicon/TeiLexicon.cs
@@ -150,79 +150,16 @@ public class TeiLexicon : ILexicon
 
                     var usg = MapDialectCode(elem.ElementLocal("usg")?.Value);
 
-                    forms.Add(new Form(formType, usg, orth));
+                    var subGrammarGroupElem = elem.ElementLocal("gramGrp");
+                    GrammarGroup? subGrammarGroup = subGrammarGroupElem is not null
+                        ? ParseGrammarGroup(subGrammarGroupElem)
+                        : null;
+
+                    forms.Add(new Form(formType, usg, orth, subGrammarGroup));
                     break;
 
                 case "gramGrp":
-                    var subc = elem.ElementLocal("subc")?.Value;
-                    var note = elem.ElementLocal("note")?.Value;
-
-                    PartOfSpeech pos = elem.ElementLocal("pos")?.Value?.TrimEnd('?') switch
-                    {
-                        "Subst." => PartOfSpeech.Substantive,
-                        "Vb." => PartOfSpeech.Verb,
-                        "Adv." => PartOfSpeech.Adverb,
-                        "Adj." => PartOfSpeech.Adjective,
-                        "Präp." => PartOfSpeech.Preposition,
-                        "Pron." => PartOfSpeech.Pronoun,
-                        "Poss. Pron." => PartOfSpeech.PossessivePronoun,
-                        "Interr. Pron." => PartOfSpeech.InterrogativePronoun,
-                        "Dem. Pron." => PartOfSpeech.DemonstrativePronoun,
-                        "Selbst. Pers. Pron." => PartOfSpeech.PersonalPronoun,
-                        "Konj." => PartOfSpeech.Conjugation,
-                        "bestimmter Artikel" => PartOfSpeech.DefiniteArticle,
-                        "Zahlzeichen" => PartOfSpeech.NumberSign,
-                        "Zahlwort" => PartOfSpeech.Numeral,
-                        "Possessivartikel" => PartOfSpeech.PossessiveArticle,
-                        "Suffixpronomen" => PartOfSpeech.PronounSuffix,
-                        "Präfix" => PartOfSpeech.Prefix,
-                        "Nominalpräfix" => PartOfSpeech.NominalPrefix,
-                        "Verbalpräfix" => PartOfSpeech.VerbalPrefix,
-                        "Adjektivpräfix" => PartOfSpeech.AdjectivePrefix,
-                        "Präfixpronomen (Präsens I)" => PartOfSpeech.PronounPrefixPresent1,
-                        "Präfix der Ordinalzahlen" => PartOfSpeech.OrdinalPrefix,
-                        "Possessivpräfix" => PartOfSpeech.PossessivePrefix,
-                        "Konjunktiv" => PartOfSpeech.Conjunctive,
-                        "Partikel" or "Partikel, enklitisch" => PartOfSpeech.Particle,
-                        "Satzkonverter" => PartOfSpeech.SentenceConverter,
-                        "Interjektion" => PartOfSpeech.Interjection,
-                        "Kompositum" => PartOfSpeech.Composite,
-                        "unpersönlicher Ausdruck" => PartOfSpeech.ImpersonalExpression,
-                        "" => PartOfSpeech.Unknown,
-                        _ => throw new NotImplementedException(),
-                    };
-
-                    var gen = elem.ElementLocal("gen")?.Value switch
-                    {
-                        "m." => Gender.Masculine,
-                        "f." => Gender.Feminine,
-                        _ => Gender.Neutral,
-                    };
-
-                    var num = elem.ElementLocal("number")?.Value switch
-                    {
-                        "sg." => Number.Singular,
-                        "pl." => Number.Plural,
-                        _ => Number.None,
-                    };
-
-                    List<GrammarEntry> grammarEntries = new();
-                    foreach (var grammarEntryElem in elem.ElementsLocal("gram"))
-                    {
-                        var grammarType = grammarEntryElem.AttributeLocal("type")?.Value switch
-                        {
-                            "collocPrep" => GrammarType.CollocPrep,
-                            "collocAdv" => GrammarType.CollocAdv,
-                            "collocPartikel" or "collocParticle" => GrammarType.CollocParticle,
-                            "collocNoun" => GrammarType.CollocNoun,
-                            "collocConj" => GrammarType.CollocConj,
-                            _ => throw new NotImplementedException()
-                        };
-
-                        grammarEntries.Add(new(grammarType, grammarEntryElem.Value));
-                    }
-
-                    grammarGroup = new(pos, num, gen, grammarEntries, subc, note);
+                    grammarGroup = ParseGrammarGroup(elem);
                     break;
 
                 case "sense":
@@ -245,6 +182,79 @@ public class TeiLexicon : ILexicon
         }
 
         return new(id!, type, forms, senses, grammarGroup!);
+    }
+
+    private static GrammarGroup ParseGrammarGroup(XElement elem)
+    {
+        var subc = elem.ElementLocal("subc")?.Value;
+        var note = elem.ElementLocal("note")?.Value;
+
+        PartOfSpeech pos = elem.ElementLocal("pos")?.Value?.TrimEnd('?') switch
+        {
+            "Subst." => PartOfSpeech.Substantive,
+            "Vb." => PartOfSpeech.Verb,
+            "Adv." => PartOfSpeech.Adverb,
+            "Adj." => PartOfSpeech.Adjective,
+            "Präp." => PartOfSpeech.Preposition,
+            "Pron." => PartOfSpeech.Pronoun,
+            "Poss. Pron." => PartOfSpeech.PossessivePronoun,
+            "Interr. Pron." => PartOfSpeech.InterrogativePronoun,
+            "Dem. Pron." => PartOfSpeech.DemonstrativePronoun,
+            "Selbst. Pers. Pron." => PartOfSpeech.PersonalPronoun,
+            "Konj." => PartOfSpeech.Conjugation,
+            "bestimmter Artikel" => PartOfSpeech.DefiniteArticle,
+            "Zahlzeichen" => PartOfSpeech.NumberSign,
+            "Zahlwort" => PartOfSpeech.Numeral,
+            "Possessivartikel" => PartOfSpeech.PossessiveArticle,
+            "Suffixpronomen" => PartOfSpeech.PronounSuffix,
+            "Präfix" => PartOfSpeech.Prefix,
+            "Nominalpräfix" => PartOfSpeech.NominalPrefix,
+            "Verbalpräfix" => PartOfSpeech.VerbalPrefix,
+            "Adjektivpräfix" => PartOfSpeech.AdjectivePrefix,
+            "Präfixpronomen (Präsens I)" => PartOfSpeech.PronounPrefixPresent1,
+            "Präfix der Ordinalzahlen" => PartOfSpeech.OrdinalPrefix,
+            "Possessivpräfix" => PartOfSpeech.PossessivePrefix,
+            "Konjunktiv" => PartOfSpeech.Conjunctive,
+            "Partikel" or "Partikel, enklitisch" => PartOfSpeech.Particle,
+            "Satzkonverter" => PartOfSpeech.SentenceConverter,
+            "Interjektion" => PartOfSpeech.Interjection,
+            "Kompositum" => PartOfSpeech.Composite,
+            "unpersönlicher Ausdruck" => PartOfSpeech.ImpersonalExpression,
+            "" => PartOfSpeech.Unknown,
+            _ => throw new NotImplementedException(),
+        };
+
+        var gen = elem.ElementLocal("gen")?.Value switch
+        {
+            "m." => Gender.Masculine,
+            "f." => Gender.Feminine,
+            _ => Gender.Neutral,
+        };
+
+        var num = elem.ElementLocal("number")?.Value switch
+        {
+            "sg." => Number.Singular,
+            "pl." => Number.Plural,
+            _ => Number.None,
+        };
+
+        List<GrammarEntry> grammarEntries = new();
+        foreach (var grammarEntryElem in elem.ElementsLocal("gram"))
+        {
+            var grammarType = grammarEntryElem.AttributeLocal("type")?.Value switch
+            {
+                "collocPrep" => GrammarType.CollocPrep,
+                "collocAdv" => GrammarType.CollocAdv,
+                "collocPartikel" or "collocParticle" => GrammarType.CollocParticle,
+                "collocNoun" => GrammarType.CollocNoun,
+                "collocConj" => GrammarType.CollocConj,
+                _ => throw new NotImplementedException()
+            };
+
+            grammarEntries.Add(new(grammarType, grammarEntryElem.Value));
+        }
+
+        return new(pos, num, gen, grammarEntries, subc, note);
     }
     
     public static LanguageInfo MapDialectCode(string? dialectCode) => dialectCode switch
