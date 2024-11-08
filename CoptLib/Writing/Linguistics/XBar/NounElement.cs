8660f598986d8a8f0824ac2234bc62c626052312
Yoshi Askharoun <jjask7@gmail.com>
2024-11-07 23:42:20 -0600

    Move NounMeta and create NounElement

---
+++ CoptLib/Writing/Linguistics/XBar/NounElement.cs
@@ -0,0 +1,26 @@
+﻿using CoptLib.Writing.Lexicon;
+
+namespace CoptLib.Writing.Linguistics.XBar;
+
+public record NounElement(Range SourceRange, NounMeta Meta) : StructuralElement(SourceRange)
+{
+    public override string ToString() => $"Noun{{{SourceRange}, {Meta}}}";
+}
+
+public record NounMeta(ILexemeReference Meaning, InflectionMeta Inflection) : IMeta
+{
+    public override string ToString() => $"NOUN{{{Meaning}, {Inflection}}}";
+}
+
+public interface ILexemeReference
+{
+    string Orthography { get; }
+}
+
+// TODO: What system will be used to map words to meanings?
+public record ConceptReference(string Orthography, string ConceptNetId) : ILexemeReference;
+
+public record LexiconEntryReference(LexiconEntry Entry) : ILexemeReference
+{
+    public string Orthography => Entry.Forms[0].Orthography;
+}
